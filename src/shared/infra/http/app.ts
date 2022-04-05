58483749c9520f88df18b594a91b4b76e8564b7c
mmpdrosa <matheusmp2001@gmail.com>
2022-04-05 10:50:19 -0300

    :rocket: add aws configurations

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,8 +1,10 @@
 import 'reflect-metadata';
+import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
+import upload from '@config/upload';
 import { AppError } from '@shared/errors/AppError';
 import createConnection from '@shared/infra/typeorm';
 import '@shared/container';
@@ -16,6 +18,8 @@ const app = express();
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 
 app.use(router);
 
--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -3,15 +3,17 @@ import multer from 'multer';
 
 import uploadConfig from '@config/upload';
 import { CreateUserController } from '@modules/accounts/useCases/createUser/CreateUserController';
+import { ShowUserProfileController } from '@modules/accounts/useCases/showUserProfile/ShowUserProfileController';
 import { UpdateUserAvatarController } from '@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 import { ensureAthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
 
 const usersRoutes = Router();
 
-const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'));
+const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const showUserProfileController = new ShowUserProfileController();
 
 usersRoutes.post('/', createUserController.handle);
 usersRoutes.patch(
@@ -19,6 +21,11 @@ usersRoutes.patch(
   ensureAthenticated,
   uploadAvatar.single('avatar'),
   updateUserAvatarController.handle
+);
+usersRoutes.get(
+  '/profile',
+  ensureAthenticated,
+  showUserProfileController.handle
 );
 
 export { usersRoutes };
